b24c4968239e8c19742f30f790be4df7e4b35304
sadoyui <102640681+sadoyui@users.noreply.github.com>
2022-06-22 00:57:57 +0800

    Update exthm_haydn.mk

--- exthm_haydn.mk
+++ exthm_haydn.mk
@@ -12,7 +12,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/haydn/device.mk)
 
 # Inherit some common exthm stuff.
-$(call inherit-product, vendor/exthm/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
