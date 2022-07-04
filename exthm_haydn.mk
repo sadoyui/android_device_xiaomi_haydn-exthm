7cf413f1d30b405b185bdcb8e12e59299fcc0715
sadoyui <102640681+sadoyui@users.noreply.github.com>
2022-07-04 22:48:53 +0800

    Update exthm_haydn.mk

--- exthm_haydn.mk
+++ exthm_haydn.mk
@@ -9,6 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from haydn device
+$(call inherit-product, vendor/exthm/config/common_full_phone.mk)
 $(call inherit-product, device/xiaomi/haydn/device.mk)
 
 # Inherit some common exthm stuff.
